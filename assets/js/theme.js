f3b91c57d9efb25904b9c2798abd88708eac762e
John Stilia <stilia.johny@gmail.com>
2020-08-25 00:07:57 +0100

    add new jekyl _files and assets

---
+++ assets/js/theme.js
@@ -0,0 +1,10 @@
+const themeMap = {
+  'Theme Bright': {
+    'background-color': 'white',
+    'text-color': '#222'
+  },
+  'Theme Dark': {
+    'background-color': '#222',
+    'text-color': 'white'
+  }
+}
