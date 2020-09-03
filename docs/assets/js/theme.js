45d95966c04592d0931376a0094494797d2431b9
John Stilia <stilia.johny@gmail.com>
2020-09-03 23:31:24 +0100

    add fixed public site

---
+++ docs/assets/js/theme.js
@@ -0,0 +1,14 @@
+const themeMap = {
+  'Theme Hacker': {
+    'background-color': '#000000',
+    'text-color': '#00eb00',
+  },
+  'Theme Bright': {
+    'background-color': 'white',
+    'text-color': '#222',
+  }
+}
+
+
+
+
